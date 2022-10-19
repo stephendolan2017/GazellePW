a744342f4828738b56bd39fdbf2b4b4ff66df1aa
stephendolan2017 <99218827+stephendolan2017@users.noreply.github.com>
2022-10-19 15:27:01 +0100

    Update torrenttable.class.php

--- classes/torrenttable.class.php
+++ classes/torrenttable.class.php
@@ -602,10 +602,7 @@ class TorrentTableView {
             <? } ?>
             <? if (!empty($Description)) { ?>
                 <div class="TorrentDetail-row is-description is-block">
-                    <div class="TorrentDetailSubtitle-header" id="subtitles_box_header">
-                        <strong class="TorrentDetailSubtitle-title" id="subtitles_box_title"><?= t('server.common.subtitles') ?>:
-                        </strong>
-                    </div>
+                    <a>test</a>
                     <?= Text::full_format($Description) ?>
                 </div>
             <? } ?>
