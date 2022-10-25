8118ba097895402cd7acd3f40b045be5e659f610
stephendolan2017 <99218827+stephendolan2017@users.noreply.github.com>
2022-10-25 21:50:49 +0100

    Update torrent_form.class.php

--- classes/torrent_form.class.php
+++ classes/torrent_form.class.php
@@ -721,9 +721,10 @@ class TORRENT_FORM {
                                     <?
                                     $resolution = $TorrentResolution;
                                     $resolution_width = '';
+                                    $resolution_height = '';
                                     if ($resolution && !in_array($resolution, $this->Resolutions)) {
                                         $resolution = "Other";
-                                        list($resolution_width) = explode('&times;', $Torrent['Resolution']);
+                                        list($resolution_width, $resolution_height) = explode('&times;', $Torrent['Resolution']);
                                     }
                                     foreach (Misc::display_array($this->Resolutions) as $Resolution) {
                                         echo "\t\t\t\t\t\t<option value=\"$Resolution\"";
@@ -737,6 +738,8 @@ class TORRENT_FORM {
                                 </select>
                                 <span class="hidden">
                                     <input class="Input is-small" type="text" id="resolution_width" name="resolution_width" value="<?= $resolution_width ?>">
+                                    <span>×</span>
+                                    <input class="Input is-small" type="text" id="resolution_height" name="resolution_height" , value="<?= $resolution_height ?>">
                                 </span>
                             </div>
                         </div>
